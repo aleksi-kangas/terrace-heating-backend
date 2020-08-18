e4a32d333a0a6ac88b5d0a4f1acbb370386bf407
Aleksi Kangas <aokang@utu.fi>
2020-08-18 16:40:27 +0300

    Testing: API tests for heat pump data implemented

--- utils/config.js
+++ utils/config.js
@@ -3,8 +3,14 @@ import dotenv from 'dotenv';
 // Load .env file contents to process.env
 dotenv.config();
 
+let { MONGODB_URI } = process.env;
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+}
+
 const config = {
-  MONGODB_URI: process.env.MONGODB_URI,
+  MONGODB_URI,
   PORT: process.env.PORT,
   MODBUS_HOST: process.env.MODBUS_HOST,
   MODBUS_PORT: process.env.MODBUS_PORT,
