9d81ec9120ed4328775994cb6b290b4b5156bca6
Aleksi Kangas <aleksi.kangas@hotmail.com>
2020-09-13 18:30:44 +0300

    Bug Fix: Query strings now must form a valid date

--- services/heatPumpService.js
+++ services/heatPumpService.js
@@ -12,12 +12,15 @@ import HeatPump from '../models/heatPump.js';
  * @return {Array<Object>} - contains heat pump data entries from the given date onwards (or all)
  */
 const getData = async (date) => {
-  // If any of the properties of date are missing,
+  const dateThreshold = moment(`${date.year}-${date.month}-${date.day}`).format('YYYY-MM-DD');
+
+  // If any of the properties of date are missing or the date itself is not valid,
   // retrieve all entries of heat pump data.
-  if (!date.year || !date.month || !date.day) {
+  if (!date.year || !date.month || !date.day || !dateThreshold.isValid()) {
     return HeatPump.find({});
   }
-  // If year, month and day properties are present,
+
+  // If year, month and day properties are present and form a valid date,
   // retrieve entries of heat pump data from that date onwards.
   const dateLimit = moment(`${date.year}-${date.month}-${date.day}`).format('YYYY-MM-DD');
   return HeatPump.find({ time: { $gt: dateLimit } });
