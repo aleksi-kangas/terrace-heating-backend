072ccd6855168905d2846976be89ad33570abb71
Aleksi Kangas <aleksi.kangas@hotmail.com>
2020-12-10 14:40:53 +0200

    Doc: Documentation improved

--- utils/modBus.js
+++ utils/modBus.js
@@ -3,7 +3,12 @@ import config from './config.js';
 import HeatPump from '../models/heatPump.js';
 
 /**
- * Helper function for signing an unsigned number.
+ * Contains logic for communicating with the heat pump via ModBus-protocol.
+ * Defined functions can be modified to achieve desired functionality.
+ */
+
+/**
+ * Helper function for signing an unsigned 16 bit number.
  * @param value - number to be signed
  * @return {number} - the signed representation of the input value
  */
@@ -36,23 +41,37 @@ const parseModBusQuery = (data) => ({
   heatDistCircuitTemp3: signUnsignedValues(data[116]),
 });
 
+// Connect to the heat pump via ModBus-protocol
 const client = new ModBus();
 client.connectTCP(config.MODBUS_HOST, { port: config.MODBUS_PORT })
   .then();
 
+/**
+ * Queries predetermined registers from the heat pump and saves the queried data to MongoDB.
+ * @return {Object} - contains saved data
+ */
 const queryHeatPumpValues = async () => {
   const values = await client.readHoldingRegisters(1, 120);
-  // Parse queried data and save it to MongoDB
   const parsedData = parseModBusQuery(values.data);
   const heatPumpData = new HeatPump(parsedData);
   return heatPumpData.save();
 };
 
+/**
+ * Queries the number of active heat distribution circuits from the heat pump.
+ * Reasonable return values are 2 and 3.
+ * @return {Object} - number of active heat distribution circuits (usually 2 or 3)
+ */
 const queryNumberOfActiveCircuits = async () => {
   const activeCircuits = await client.readHoldingRegisters(5100, 1);
   return activeCircuits.data[0];
 };
 
+/**
+ * Activates/de-activates the third heat distribution circuit of the heat pump.
+ * Activation/de-activation is determined by the current state of the heat distribution circuits.
+ * Either from 2 -> 3 or 3 -> 2.
+ */
 const toggleCircuitThree = async () => {
   const activeCircuits = await queryNumberOfActiveCircuits();
   const value = activeCircuits === 3 ? 2 : 3;
