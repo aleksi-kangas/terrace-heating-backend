43aafcc50d3be30eac277b086af7f72976677ad5
Aleksi Kangas <aleksi.kangas@hotmail.com>
2021-01-22 10:57:29 +0200

    Config: Added DEV database env variable

--- utils/config.js
+++ utils/config.js
@@ -5,6 +5,10 @@ dotenv.config();
 
 let { MONGODB_URI } = process.env;
 
+if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.DEV_MONGODB_URI;
+}
+
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
