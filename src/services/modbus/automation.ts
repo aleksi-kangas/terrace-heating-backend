a510909a829e14f7772481f5b10e909e0fb5a5c3
Aleksi Kangas <aleksi.kangas@hotmail.com>
2021-04-13 22:08:25 +0300

    Refactor: Automation functionality separated into helper-functions + minor logging improvement

--- src/services/modbus/automation.ts
+++ src/services/modbus/automation.ts
@@ -3,6 +3,7 @@ import HeatPump from '../../models/heatPump';
 import ModBusApi from './api';
 import { signValue } from './helpers';
 import Logger from '../../utils/logger';
+import { HeatPumpEntry } from '../../types';
 
 // Holds at maximum 5 values of difference in estimated minutes left till upper limit is reached
 let buffer: number[] = [];
@@ -18,6 +19,35 @@ const calculateAverage = () => {
   const sum = buffer.reduce((s: number, estMinutesLeftDiff: number) => s + estMinutesLeftDiff, 0);
   return sum / buffer.length;
 };
+
+// Calculate temperature deltas for lower and upper tank
+/**
+ * Helper function for calculating temperature deltas of the tanks between the last two entries.
+ * @param thisEntry the most recent entry of heat-pump data
+ * @param previousEntry the second most recent entry of heat-pump data
+ */
+const temperatureDeltas = (thisEntry: HeatPumpEntry, previousEntry: HeatPumpEntry) => ({
+  lowerTankTempDelta: Math.round((
+    thisEntry.lowerTankTemp - previousEntry.lowerTankTemp + Number.EPSILON) * 100) / 100,
+  upperTankTempDelta: Math.round((
+    thisEntry.upperTankTemp - previousEntry.upperTankTemp + Number.EPSILON) * 100) / 100,
+});
+
+/**
+ * Helper function for calculating the estimated amount of minutes for each tank to reach its upper limit.
+ * @param thisEntry the most recent entry of heat-pump data
+ * @param tempDeltas object containing current temperature deltas for the tanks created by temperatureDeltas()-function
+ */
+const estimatedTimesLeftUntilUpperLimits = (
+  thisEntry: HeatPumpEntry, tempDeltas: { lowerTankTempDelta: number, upperTankTempDelta: number },
+) => ({
+  lowerTank: Math.round(
+    ((thisEntry.lowerTankUpperLimit - thisEntry.lowerTankTemp) / tempDeltas.lowerTankTempDelta + Number.EPSILON) * 100,
+  ) / 100,
+  upperTank: Math.round(
+    ((thisEntry.upperTankUpperLimit - thisEntry.upperTankTemp) / tempDeltas.upperTankTempDelta + Number.EPSILON) * 100,
+  ) / 100,
+});
 
 /**
  * Automatically changes the heat exchanger ratio of the heat-pump,
@@ -37,37 +67,28 @@ export const automatedHeatExchangerRatio = async (): Promise<void> => {
       && thisEntry.upperTankTemp > thisEntry.upperTankUpperLimit) return;
 
     // Calculate temperature deltas for lower and upper tank
-    const lowerTankTempDelta = Math.round((
-      thisEntry.lowerTankTemp - previousEntry.lowerTankTemp + Number.EPSILON) * 100) / 100;
-    const upperTankTempDelta = Math.round((
-      thisEntry.upperTankTemp - previousEntry.upperTankTemp + Number.EPSILON) * 100) / 100;
+    const tempDeltas = temperatureDeltas(thisEntry, previousEntry);
 
     // Adjusting is not needed when temperature change is negative
-    if (lowerTankTempDelta < 0 || upperTankTempDelta < 0) {
+    if (tempDeltas.lowerTankTempDelta < 0 || tempDeltas.upperTankTempDelta < 0) {
       // Clear the buffer
       buffer = [];
       return;
     }
 
     // Estimated minutes left until temperature reaches upper limit
-    const estMinutesTillUpperLimit = {
-      lowerTank: Math.round(
-        ((thisEntry.lowerTankUpperLimit - thisEntry.lowerTankTemp) / lowerTankTempDelta + Number.EPSILON) * 100,
-      ) / 100,
-      upperTank: Math.round(
-        ((thisEntry.upperTankUpperLimit - thisEntry.upperTankTemp) / upperTankTempDelta + Number.EPSILON) * 100,
-      ) / 100,
-    };
+    const estMinutesUntilUpperLimit = estimatedTimesLeftUntilUpperLimits(thisEntry, tempDeltas);
 
     // Keep the buffer at max size 5
     if (buffer.length === 5) {
       buffer.shift();
     }
-    buffer.push(estMinutesTillUpperLimit.lowerTank - estMinutesTillUpperLimit.upperTank);
+    buffer.push(estMinutesUntilUpperLimit.lowerTank - estMinutesUntilUpperLimit.upperTank);
 
-    // Check if adjusting threshold is exceeded
+    // Average of (max 5) differences in estimated minutes until tanks reach their limits
     const average = calculateAverage();
 
+    // Adjust only when there are 5 consecutive positive temperature deltas and the adjusting threshold is exceeded
     if (buffer.length === 5 && Math.abs(average) >= adjustmentThreshold) {
       const heatExchangerRatio = signValue(await ModBusApi.queryHeatExchangerRatio());
       if (average < 0) {
@@ -75,6 +96,8 @@ export const automatedHeatExchangerRatio = async (): Promise<void> => {
         if (heatExchangerRatio !== newHeatExchangerRatio) {
           Logger.info(`Heat exchanger ratio increased to ${newHeatExchangerRatio} at ${moment.now()}`);
           await ModBusApi.setHeatExchangerRatio(newHeatExchangerRatio);
+        } else {
+          Logger.info(`Heat exchanger ratio is already at the upper bound (50) - ${moment.now()}`);
         }
       }
       if (average > 0) {
@@ -82,6 +105,8 @@ export const automatedHeatExchangerRatio = async (): Promise<void> => {
         if (heatExchangerRatio !== newHeatExchangerRatio) {
           Logger.info(`Heat exchanger ratio decreased to ${newHeatExchangerRatio} at ${moment.now()}`);
           await ModBusApi.setHeatExchangerRatio(newHeatExchangerRatio);
+        } else {
+          Logger.info(`Heat exchanger ratio is already at the lower bound (10) - ${moment.now()}`);
         }
       }
       // Clear buffer
